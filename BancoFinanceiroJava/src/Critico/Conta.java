19efafbf10e140189f06f89dee7cc00af4429e9e
Luiz Carlos <luizcsoaresneto@gmail.com>
2020-07-03 23:41:20 -0300

    primeiro commit

---
+++ BancoFinanceiroJava/src/Critico/Conta.java
@@ -0,0 +1,37 @@
+package Critico;
+
+public class Conta {
+	String titular;
+	int numero;
+	String agencia;
+	double saldo;
+	String data;
+	
+	boolean saca(double valor){
+		
+		if(valor <= this.saldo){
+			
+			this.saldo-=valor;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	boolean deposita(double valor){
+		if(valor > 0){
+			this.saldo+=valor;
+			return true;
+		}
+		return false;
+	}
+	
+	boolean transfere(Conta contaBanco, double valor){
+		if(this.saca(valor)){
+			
+			return contaBanco.deposita(valor);
+		}else {
+			return false;
+		}
+	}
+}
---
+++ BancoFinanceiroJava/src/Critico/Main.java
@@ -0,0 +1,10 @@
+package Critico;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
